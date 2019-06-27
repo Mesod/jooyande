878eed9c15346040f821278796ac6c0f17b92435
Masood Adeli <masood.adeli@reyhoon.com>
2019-06-27 13:22:25 +0430

    add editor config. update gitignore.

--- jooyande/app.py
+++ jooyande/app.py
@@ -1,5 +1,19 @@
 from jooyande.config import config
+from telethon import TelegramClient, events, sync
 
 def run():
     conf = config.TelegramConfig()
-    print(conf.api_hash)
+    client = TelegramClient('session_name', conf.api_id, conf.api_hash)
+    client.start()
+    print(client.get_me().stringify())
+
+    client.send_message('username', 'Hello! Talking to you from Telethon')
+    client.send_file('username', '/home/myself/Pictures/holidays.jpg')
+
+    client.download_profile_photo('me')
+    messages = client.get_messages('username')
+    messages[0].download_media()
+
+    @client.on(events.NewMessage(pattern='(?i)hi|hello'))
+    async def handler(event):
+        await event.respond('Hey!')
